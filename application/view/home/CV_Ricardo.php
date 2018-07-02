fad26995cda8068be57d9de7753cf8db2593ba05
dejipapi <ricardo.garcia.cuellar@gmail.com>
2018-07-01 23:26:03 -0500

    Actualizacion de estilos
    Se actualizo el estilo para ek CV de Ricardo Garcia.

--- application/view/home/CV_Ricardo.php
+++ application/view/home/CV_Ricardo.php
@@ -27,7 +27,7 @@
 	<div class="container">
  
 	<header role="banner">
-		<div class="col-lg-11">
+		<div class="col-md-12">
 			<div class="row">
 				<div class="col-md-10 col-md-offset-2">
 				<hgroup>
@@ -41,182 +41,213 @@
 			</div>
 		</div>
 	</header>
-	
- 	<section class="hide">
-		<div class="col-lg-11">
-			<h3>Contactame</h3>
-			<p>Completa el formulario y me comunicare contigo lo antes posible. 
-			<br><em>Todos los campos son requeridos</em></p>
-			<form novalidate="novalidate" method="post" action="#" name="contact" class="grid_16">
-				<p class="grid_10"><textarea name="message" placeholder="Votre message" class="required"></textarea></p>
-				<p class="grid_6">
-					<input name="nom" placeholder="Nom - Prénom" class="required" type="text">
-					<input name="email" placeholder="Adresse email" class="required" type="email">	
-					<input name="envoi" value="Envoyer le message" class="required" type="submit">
-					<span class="messageform"></span>
-				</p>
-			</form>
-		</div>
-	</section> 
 	<div class="row graybackground">
-	<section   class="col-md-9 col-md-offset-1 ">
+	<section   class="col-md-12">
 		<div class="row"> 
 			<div class="col-md-6 col-xs-12">
-				 
-				
-				 
-				<h3><i class="small material-icons">account_circle</i> Resumen</h3>
-				 
-				<p>Ingeniero en sistemas computacionales especializado en desarrollo web con una gran experiencia en el campo profesional de servicios a la medida de las necesidades del cliente, con el compromiso de siempre brindar lo mejos.</p>
+				<div class="col-md-12">
+					<h3><i class="small material-icons">account_circle</i> Resumen</h3>
+					<p>Ingeniero en sistemas computacionales especializado en desarrollo web con una gran experiencia en el campo profesional de servicios a la medida de las necesidades del cliente, con el compromiso de siempre brindar lo mejos.</p>
+				</div>
 			</div>
 			 
-			<div class=" col-md-5 col-xs-12">
-				<h3><i class="small material-icons">computer</i>  Habilidades </h3>
-				<ul class="barres">
-					<li data-skills="17">C# <span style="width: 40%;"></span></li>
-					<li data-skills="17">PHP<span style="width: 90%;"></span></li>
-					<li data-skills="17">SQL<span style="width: 85%;"></span></li>
-					<li data-skills="1">Boostrap<span style="width: 50%;"></span></li>
-					<li data-skills="17">Oracle<span style="width: 50%;"></span></li>
-					<li data-skills="17">MySQL<span style="width: 90%;"></span></li>
-					<li data-skills="17">JQuery<span style="width: 75%;"></span></li>
-					<li data-skills="17">Java<span style="width: 60%;"></span></li>
-				</ul>
+			<div class=" col-md-6 col-xs-12">
+				<div class="col-md-12">
+					<h3><i class="small material-icons">computer</i>  Habilidades </h3>
+					<ul class="barres">
+						<li data-skills="17">C# <span style="width: 40%;"></span></li>
+						<li data-skills="17">PHP<span style="width: 90%;"></span></li>
+						<li data-skills="17">SQL<span style="width: 85%;"></span></li>
+						<li data-skills="1">Boostrap<span style="width: 50%;"></span></li>
+						<li data-skills="17">Oracle<span style="width: 50%;"></span></li>
+						<li data-skills="17">MySQL<span style="width: 90%;"></span></li>
+						<li data-skills="17">JQuery<span style="width: 75%;"></span></li>
+						<li data-skills="17">Java<span style="width: 60%;"></span></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 		<div class="row"> 
 			<div secc class="col-md-12 ">
-				<h3><i class="small material-icons">business</i>Experiencia</h3>
-				<div class="row">	
-					<div class="col-md-11">			
-						<h4><strong>Desarrollador Web </strong> Grupo Escalante</h4>
-					</div>
-					<div class="col-md-11">
-						<p> * Mapeo de procesos para nuevos sistemas o funcionalidades.</p>
-						<p> * Utilizacipon de herramientas para la realización de diagramas UML.</p>
-						<p> * Desarrollo de reportes y graficas a medida de los clientes.</p>
-						<p> * Planeación y administración de bases de datos.</p>
-						<p> * Desarrolo de sistemas Web basados en PHP, Javascript y Java.</p>
-					</div>
-				</div> 
-				<div class="row">	
-					<div class="col-md-11">			
-						<h4><strong>Administrador de Bases de datos </strong> Att Maximus</h4>
-					</div>
-					<div class="col-md-11">
-						<p> * Administrador de base de datos del sistema de peaje del Tren electrico  en la CDMX.</p>
-						<p> * Generación de reporten mensuales para el cliente.</p>
-						<p> * Analista de información</p>
-					</div>
+				<div class="col-md-12">
+					<h3><i class="small material-icons">business</i>Experiencia</h3>
 				</div>
-				<div class="row">	
-					<div class="col-md-11">			
-						<h4><strong>Programador </strong> Pro-Innovación</h4>
-					</div>
-					<div class="col-md-11">
-						<p> * Administrador de servidor web</p>
-						<p> * Gestión de dominios, correos electronicos, FTP</p>
-						<p> * Desarrollo y mantenimiento de sitios web con Wordpress</p>
-						<p> * Desarrollo de aplicaciones de escritorios con .NET</p>
-					</div>
-				</div>  
+				<div class="col-md-11">			
+					<h4><strong>Desarrollador Web </strong></h4>
+				</div>
+				<div class="col-md-4">
+					<span class="lugar">Grupo Escalante</span>
+				</div>
+				<div class="col-md-7">
+					<span class="dates">Noviembre 2015 - Actual</span>
+				</div>
+				<div class="col-md-12">
+					<ul class="collection">
+				      <li class="collection-item">Mapeo de procesos para nuevos sistemas o funcionalidades.</li>
+				      <li class="collection-item">Utilización de herramientas para la realización de diagramas UML.</li>
+				      <li class="collection-item">Desarrollo de reportes y graficas a medida de los clientes.</li>
+				      <li class="collection-item">Planeación y administración de bases de datos.</li>
+				      <li class="collection-item">Desarrollo de sistemas Web basados en PHP, Javascript y Java.</li>
+				    </ul>
+				</div>
+			</div> 
+		</div>
+		<div class="row">	
+			<div class="col-md-12">		
+				<div class="col-md-11">		
+					<h4><strong>Administrador de Bases de datos </strong></h4>
+				</div>
+				<div class="col-md-4">
+					<span class="lugar">Att Maximus</span>
+				</div>
+				<div class="col-md-7">
+					<span class="dates">Marzo 2013 - Diciembre 2015</span>
+				</div>
+				<div class="col-md-12">
+					<ul class="collection">
+				      <li class="collection-item">Administrador de base de datos del sistema de peaje del Tren electrico  en la CDMX.</li>
+				      <li class="collection-item">Generación de reporten mensuales para el cliente.</li>
+				      <li class="collection-item">Analista de información.</li>
+				    </ul>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 ">
+				<div class="col-md-11">			
+					<h4><strong>Programador</strong></h4>
+				</div>
+				<div class="col-md-4">
+					<span class="lugar">Pro-Innovación</span>
+				</div>
+				<div class="col-md-7">
+					<span class="dates">Agosto 2012 - Marzo 2013</span>
+				</div>
+				<div class="col-md-12">
+					<ul class="collection">
+				      <li class="collection-item">Administrador de servidor web.</li>
+				      <li class="collection-item">Gestión de dominios, correos electronicos, FTP.</li>
+				      <li class="collection-item">Desarrollo y mantenimiento de sitios web con Wordpress.</li>
+				      <li class="collection-item">Desarrollo de aplicaciones de escritorios con .NET</li>
+				    </ul>
+				</div>
 			</div>
 		</div>
 		
 		<div class="row">
-			<div secc class="col-md-12 ">
-				<h3>Educación</h3>
+			<div class="col-md-12 ">
+				<div class="col-md-12">
+					<h3><i class="small material-icons">book</i> Educación</h3>
+				</div>
+				<div class="col-md-12">
+					<ul class="collection">
+				      <li class="collection-item">
+				      	<span class="title">Ingeniería en Sistemas Computacionales</span>
+					      <p>Tecnológico de Estudios Superiores de Ixtapaluca <br>
+					         Septiembre 2007 - Febrero 2012
+					      </p>
+				      </li>
+				    </ul>
+				</div> 
 			</div>
-			<div class="col-md-12">
-						<h4><strong>Ingeneria en Sistemas Computacionales</strong> Tecnológico de estudios Superiores Ixtapaluca</h4>
-			</div>
-			<div class="col-md-4">
-						<span class="lugar">Licenciatura </span>
-			</div>
-				<div class="col-md-7">
-						<span class="dates">Septiembre 2007 – Febrero 2012</span>
-					</div>
 		</div>
 		
 		<div class="row"> 
-			<div class="col-md-7">
-				<h3>Manejo del Conocimiento </h3>
-				<div secc class="col-md-12 ">
-					<h4><strong>PHP</strong></h4>
-				</div>
-				<div class="col-md-6">
-					<span class="lugar">Usado actualmente </span>
+			<div class="col-md-12">
+				<h3><i class="small material-icons">free_breakfast</i> Manejo del conocimiento</h3>
+			</div>
+			<div class="col-xs-12 col-md-4 col-lg-3">
+				<div class="card">
+					<div class="card-title center">
+	             		<p>PHP</p>
+	             	</div>
+					<div class="card-content center">
+						<p>Usado actualmente.</p>
 					</div>
-				<div class="col-md-6">
-					<span class="dates">8 años de uso  </span>
-				</div>
-				<div secc class="col-md-12 ">
-					<h4><strong>Java</strong></h4>
-				</div>
-				<div class="col-md-6">
-					<span class="lugar">Usado actualmente </span>
+					<div class="card-action center">
+						<a href="#">8 años de uso</a>
 					</div>
-				<div class="col-md-6">
-					<span class="dates">3 años de uso  </span>
-				</div>
-				<div secc class="col-md-12 ">
-					<h4><strong>Javascript</strong></h4>
-				</div>
-				<div class="col-md-6">
-					<span class="lugar">Usado actualmente </span>
+	            </div>
+			</div>
+			<div class="col-xs-12 col-md-4 col-lg-3">
+				<div class="card">
+					<div class="card-title center">
+	             		<p>JAVA</p>
+	             	</div>
+					<div class="card-content center">
+						<p>Usado actualmente.</p>
 					</div>
-				<div class="col-md-6">
-					<span class="dates">5 años de uso  </span>
-				</div>
-				<div secc class="col-md-12 ">
-					<h4><strong>.NET</strong></h4>
-				</div>
-				<div class="col-md-6">
-					<span class="lugar">No usado actualmente </span>
+					<div class="card-action center">
+						<a href="#">3 años de uso</a>
 					</div>
-				<div class="col-md-6">
-					<span class="dates">2 años de uso  </span>
-				</div>
-				<div secc class="col-md-12 ">
-					<h4><strong>MVC</strong></h4>
-				</div>
-				<div class="col-md-6">
-					<span class="lugar">Usado actualmente </span>
+	            </div>
+			</div>
+			<div class="col-xs-12 col-md-4 col-lg-3">
+				<div class="card">
+					<div class="card-title center">
+	             		<p>javascript</p>
+	             	</div>
+					<div class="card-content center">
+						<p>Usado actualmente.</p>
 					</div>
-				<div class="col-md-6">
-					<span class="dates">3 años de uso  </span>
-				</div>
-				<div secc class="col-md-12 ">
-					<h4><strong>Bootstrap</strong></h4>
-				</div>
-				<div class="col-md-6">
-					<span class="lugar">No usado actualmente </span>
+					<div class="card-action center">
+						<a href="#">5 años de uso</a>
 					</div>
-				<div class="col-md-6">
-					<span class="dates">3 años de uso  </span>
+	            </div>
+			</div>
+			<div class="col-xs-12 col-md-4 col-lg-3">
+				<div class="card">
+					<div class="card-title center">
+	             		<p>C#</p>
+	             	</div>
+					<div class="card-content center">
+						<p>No usado actualmente.</p>
+					</div>
+					<div class="card-action center">
+						<a href="#">2 años de uso</a>
+					</div>
+	            </div>
+			</div>
+			<div class="col-xs-12 col-md-4 col-lg-3">
+				<div class="card">
+					<div class="card-title center">
+	             		<p>Bootstrap</p>
+	             	</div>
+					<div class="card-content center">
+						<p>Usado actualmente.</p>
+					</div>
+					<div class="card-action center">
+						<a href="#">3 años de uso</a>
+					</div>
+	            </div>
+			</div>
+		</div>	
+		<!-- Contact -->
+		 <div class="row Contact">
+	    	<div class="col-md-12">
+				<h3><i class="small material-icons">person_pin</i> Contacto</h3>
+			</div>
+			<div class="col-md-12">
+				<div class="card">
+					<div class="col-md-12 col-sm-6">
+						<img src="<?php echo URL; ?>img/map-marker.png"><span>Mexico, Estado de México</span>
+					</div> 
+					<div class="col-md-12 col-sm-6">
+						<img src="<?php echo URL; ?>img/skype.png"><span>ricardo.geek.garcia</span>
+					</div> 
+					<div class="col-md-12 col-sm-6">
+						<img src="<?php echo URL; ?>img/whatsapp.png" target="_blank">
+						<span><a href="https://api.whatsapp.com/send?phone=+5215583224784&text=Hola,me gustaria contratar tus servicios?">5583224784</a></span>
+					</div>
+					<div class="col-md-12 col-sm-6">
+						<img src="<?php echo URL; ?>img/gmail.png"> <span ><a href="mailto:ricardo.garcia.cuellar@gmail.com">ricardo.garcia.cuellar@gmail.com</a></span>
+					</div> 
+					<div class="col-md-12 col-sm-6">
+						<img src="<?php echo URL; ?>img/web.png"> <span ><a href="https://rookye.com.mx">Visita mi sitio web personal</a></span>
+					</div>	 
 				</div>
 			</div>
-	 
-		
-			<!-- Contact -->
-			<div class="col-md-5 Contact">
-				<h3>Contacto</h3>
-				
-				 <div class="col-md-12 col-sm-6">
-					<img src="<?php echo URL; ?>img/map-marker.png"> <span >  Mexico, Estado de México</span> 
-				</div> <div class="col-md-12 col-sm-6">
-					<img src="<?php echo URL; ?>img/skype.png"><span > ricardo.geek.garcia</span>
-				</div> <div class="col-md-12 col-sm-6">
-					<img src="<?php echo URL; ?>img/whatsapp.png" target="_blank"><a href="https://api.whatsapp.com/send?phone=+521558322 4784&text=Hola,me gustaria contratar tus servicios?">5583224784</a>
-				</div> <div class="col-md-12 col-sm-6">
-					<img src="<?php echo URL; ?>img/gmail.png"> <span ><a href="mailto:elcorreo@gmails.com">Mi correo</a></span>
-				</div> <div class="col-md-12 col-sm-6">
-					<img src="<?php echo URL; ?>img/web.png"> <span ><a href="http://www.rookye.com.mx">http://www.rookye.com.mx</a></span>
-				</div>
-					 
-				 
-			</div>
-		</div>
+	    </div>
 	</section>
 </div>
  
